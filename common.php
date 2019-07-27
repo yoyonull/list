c82b5cfff82644c5445d1c9aeef0ce1c1ca5bba9
yoyonull <nimgmengweio@163.com>
2019-07-27 17:40:50 +0800

    [add]增加

---
+++ common.php
@@ -0,0 +1,7 @@
+<?php
+if(!function_exists('p')){
+	function p($data){
+		echo '<pre>';
+		print_r($data);
+	}
+}
---
+++ test.php
@@ -0,0 +1,6 @@
+<?php
+include "./common.php";
+$data = [
+	[1,2],[3,4],[5,6]
+];
+p($data);
